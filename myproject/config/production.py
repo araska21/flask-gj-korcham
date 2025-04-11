0dd0cbd23faa9c74ac78b828a505b0f8522f5199
araska21 <dya760@gmail.com>
2025-04-11 09:07:50 +0900

    flask

--- myproject/config/production.py
+++ myproject/config/production.py
@@ -2,4 +2,4 @@ from config.default import *
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = b'OC\xc7A\xaf\xe8\xe43\x1b\x1d9\x08\xe6?\xca\xc8'
+SECRET_KEY = b'u\xbcq\xfc\xb4\x87\xa5N\xb6\xfc;\n\xcaq0\x08'
